7622f3aca7748a916c98bf74a4a48d699f2828d2
universe119 <ruddh119@gmail.com>
2024-10-01 14:04:02 +0900

    스니펫 설정 추가

---
+++ src/Test.jsx
@@ -0,0 +1,14 @@
+// 대문자로 했을 mycomp 입력 시
+// export default function Test(){
+//   return(
+//     <div className='Test'>Test</div>
+//   );
+// }
+
+// 소문자로 했을 mycomp 입력 시
+export default function Test(){
+  return(
+    <div className='test'>Test</div>
+  );
+}
+// 스니펫 설정하는 방법 vscode 설정 클릭해서 Snippets 클릭해서 new 파일해서 만들면 되고 수정도 따로할 수 있음
