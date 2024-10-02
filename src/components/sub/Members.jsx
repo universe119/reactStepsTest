f4e367bc3e279722af6a5e0cfc6e1c44ad9daad9
universe119 <ruddh119@gmail.com>
2024-10-02 14:39:57 +0900

    멤버 데이터 반복 출력

--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -1,9 +1,20 @@
-import Layout from "../common/Layout";
+import Layout from '../common/Layout';
+import memberData from '../../data/memberData';
 
 export default function Members() {
+	console.log(memberData);
 	return (
-		<Layout title={"MEMBERS"}>
-			<p>Members Page Contents Come Here.</p>
+		<Layout title={'MEMBERS'}>
+			<p>Members Page contents come here.</p>
+			{/* <article>
+				<div className="pic">
+					<img src="이미지" alt="" />
+				</div>
+				<div className="txt">
+					<h2>사람 이름</h2>
+					<p>직책</p>
+				</div>
+			</article> */}
 		</Layout>
 	);
 }
