c3897f667cc4a1a272e957509427295122260df7
universe119 <ruddh119@gmail.com>
2024-10-02 11:38:43 +0900

    메뉴 배열 데이터 반복처리

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
 	const gnbArr = ["members","gallery","youtube","contact","posts"];
+	const snsArr = [FaYoutube, FaInstagramSquare, FaEnvelope];
+
 	return (
 		<header className='header'>
 			<h1>
@@ -22,35 +24,43 @@ export default function Header() {
 				</ul>
 
 				<ul className='sns'>
-					<li>
-						<FaYoutube />
-					</li>
-					<li>
-						<FaInstagramSquare />
-					</li>
-					<li>
-						<FaEnvelope />
-					</li>
-					<li></li>
+					{snsArr.map((Data, idx)=>{
+						return (
+							<li key={idx}>
+								{/* snsArr에서 반복을 돌면서 Data파라미터로 전달되는 각각의 객체는 컴포넌트 함수 아래와 같이 JSX문으로 호출 가능 이때 컴포넌트 규칙에 따라 파라미터명도 대문자로 시작해서 호출 */}
+								<Data/>
+							</li>
+						)
+					})}
 				</ul>
 			</nav>
 		</header>
 	);
 }
-/* 
-	<li>
-		<Link to={'/members'}>MEMBERS</Link>
-	</li>
-	<li>
-		<Link to={'/gallery'}>GALLERY</Link>
-	</li>
-	<li>
-		<Link to={'/youtube'}>YOUTUBE</Link>
-	</li>
-	<li>
-		<Link to={'/contact'}>CONTACT</Link>
-	</li>
-	<li>
-		<Link to={'/posts'}>POSTS</Link>
-	</li> 
-*/
+
+	// <li>
+	// 	<Link to={'/members'}>MEMBERS</Link>
+	// </li>
+	// <li>
+	// 	<Link to={'/gallery'}>GALLERY</Link>
+	// </li>
+	// <li>
+	// 	<Link to={'/youtube'}>YOUTUBE</Link>
+	// </li>
+	// <li>
+	// 	<Link to={'/contact'}>CONTACT</Link>
+	// </li>
+	// <li>
+	// 	<Link to={'/posts'}>POSTS</Link>
+	// </li> 
+
+	// <li>
+	// 					<FaYoutube />
+	// 				</li>
+	// 				<li>
+	// 					<FaInstagramSquare />
+	// 				</li>
+	// 				<li>
+	// 					<FaEnvelope />
+	// 				</li>
+	// 				<li></li>
