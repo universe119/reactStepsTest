df05144c1b5afdca6404ed0d849670fbbc0fbf61
universe119 <ruddh119@gmail.com>
2024-10-02 10:42:22 +0900

    Title 컴포넌트에 디폴트 파라미터값 지정, Gallery 컴포넌트에 모든 서브페이지 컴포넌트가 공통으로 공유할 구조를 Layout컴포넌트로 적용, 레이아웃 컴포넌트 서브페이지 적용 실습 미션

---
+++ src/components/common/Layout.jsx
@@ -0,0 +1,11 @@
+export default function Layout({title}){
+  return(
+    <main className={title}>
+      <h1>{title}</h1>
+    </main>
+  );
+}
+
+//미션 (10시 42분까지)
+//위의 레이아웃 컴포넌트 구조분석
+//모든 서브페이지 컴포넌트에 위의 레이아웃 컴포넌트를 적용해서 같은 레이아웃을 공유하도록 설정
--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -3,8 +3,8 @@ import Title from "../common/Title";
 export default function Members() {
 	return (
 		<main className='members'>
-			<Title color={'red'}>
-				<strong>MEMBERS</strong>
+			<Title>
+				MEMBERS
 			</Title>
 		</main>
 	);
