168edceb785579a1e001ed0113d4a457055c0e9f
universe119 <ruddh119@gmail.com>
2024-10-02 14:32:26 +0900

    pathname을 통한 메뉴 활성화

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,63 +1,37 @@
-import { FaYoutube, FaInstagramSquare, FaEnvelope } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-
+import { FaEnvelope, FaInstagramSquare, FaYoutube } from 'react-icons/fa';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Header() {
-	const gnbArr = ["members","gallery","youtube","contact","posts"];
-	const snsArr = [FaYoutube, FaInstagramSquare, FaEnvelope];
-
+	const gnbArr = ['members', 'gallery', 'youtube', 'contact', 'posts'];
+	const snsArr = [FaEnvelope, FaInstagramSquare, FaYoutube];
+	const { pathname } = useLocation();
+	console.log(pathname);
+	//미션 (1시 20분까지)
+	//위의 pathname값을 활용해서 gnb의 li요소에 on클래스를 붙여서 메뉴명 활성화 로직 구현
 	return (
 		<header className='header'>
 			<h1>
 				<Link to={'/'}>ALPACO</Link>
 			</h1>
-
 			<nav>
 				<ul className='gnb'>
-					{gnbArr.map((data, idx)=>{
-						return(
-							<li key={idx}>
-								<Link to={"/"+data}>{data.toUpperCase()}</Link>
+					{gnbArr.map((data, idx) => {
+						return (
+							<li key={idx} className={pathname === '/' + data ? 'on' : ''}>
+								<Link to={'/' + data}>{data.toUpperCase()}</Link>
 							</li>
 						);
 					})}
 				</ul>
 
 				<ul className='sns'>
-					{/* 화살표함수 특성상 JSX반환시 { return }문은 생략 가능 */}
-					{snsArr.map((Data, idx)=>(
+					{snsArr.map((Data, idx) => (
 						<li key={idx}>
-							<Data/>
-							</li>
-							))}
+							<Data />
+						</li>
+					))}
 				</ul>
 			</nav>
 		</header>
 	);
 }
-{/* <li>
-		<Link to={'/members'}>MEMBERS</Link>
-	</li>
-	<li>
-		<Link to={'/gallery'}>GALLERY</Link>
-	</li>
-	<li>
-		<Link to={'/youtube'}>YOUTUBE</Link>
-	</li>
-	<li>
-		<Link to={'/contact'}>CONTACT</Link>
-	</li>
-	<li>
-		<Link to={'/posts'}>POSTS</Link>
-	</li> 
-
-	<li>
-		<FaYoutube />
-	</li>
-	<li>
-		<FaInstagramSquare />
-	</li>
-	<li>
-		<FaEnvelope />
-	</li>
-	<li></li> */}
