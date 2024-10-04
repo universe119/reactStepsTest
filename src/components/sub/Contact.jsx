9cbf7087b93d20e1260b204497b55748b6f3fbe5
universe119 <ruddh119@gmail.com>
2024-10-04 18:22:37 +0900

    useState, useEffect를 활용한 서버 데이터 fetching후 화면에 반복출력하는 패턴 문법(암기 필수)

--- src/components/sub/Contact.jsx
+++ src/components/sub/Contact.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
 import Layout from "../common/Layout";
 
 export default function Contact() {
+	//순서1: 서버쪽에서 받아올 데이터를 담을 빈 배열 state공간 및 함수 생성
+	const [Data, setData] = useState([]);
+
+	//순서2: 의존성배열이 비어있는 useEffect코드
+	useEffect(() => {
+		fetch("/data.json") // public에 넣어야지 되는 이유를 모르겠음
+			.then(data => data.json())
+			.then(json => {
+				console.log(json);
+				//순서3: 서버에서 가져온 데이터에서 배열만 뽑은 뒤 무조건 state에 옮겨담기
+				setData(json.data);
+			});
+	}, []);
+
 	return (
 		<Layout title={"CONTACT"}>
-			<p>Contact Page Contents Come Here.</p>
+			{Data.map((data, idx) => {
+				return (
+					// 순서4:반복시 무조건 idx로 키값 설정
+					<article key={idx}>
+						<h2>{data.name}</h2>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
