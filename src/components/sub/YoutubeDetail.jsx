66602393984ca1e5fe3b22bd675849a3a00e0454
universe119 <ruddh119@gmail.com>
2024-10-04 16:57:52 +0900

    유튜브 상세페이지 스타일링, 자가진단 항목

--- src/components/sub/YoutubeDetail.jsx
+++ src/components/sub/YoutubeDetail.jsx
@@ -46,6 +46,14 @@ export default function YoutubeDetail() {
 	);
 }
 
-//미션
-//위에서 반환받은 데이터로 상세 데이터 호출
-//제목, iframe 영상, 본문, 날짜 순으로 출력
+/*
+  자가진단 항목
+  1. useState를 이용해서 state에 값을 옮겨담고 state변경함수로 state값 변경처리
+  2. useEffect 구문의 구조를 파악하고 의존성 배열의 역할
+  3. useState와 useEffect를 활용해서 서버 데이터 fetching처리 후 state에 담기
+  4. 다이나믹 라우터를 이용해서 /youtube/:id를 활용해서 상세페이지에 특정 id값 전달하는 방법
+  5. 상태값에 있는 객체를 불러올때 ?. 형태로 옵셔널 체이닝을 처리하는 이유
+  6. 커스텀훅 만드는 방법(커스텀훅의 조건 및 호출 위치) (선택사항)
+  7. useShortenText 커스텀훅 사용방법 (선택사항)
+  8. useCombineTest 커스텀훅 사용방법 (선택사항)
+*/
