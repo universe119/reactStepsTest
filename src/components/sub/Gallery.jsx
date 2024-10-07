08964c2b53862a59e9a950d6de07974898c6a9e7
universe119 <ruddh119@gmail.com>
2024-10-07 10:40:05 +0900

    useState, useEffect훅 조합으로 외부 데이터 fetching후 화면 렌더링하는 흐름 정리, flickr API 데이터 요청후 생성된 데이터 state에 담기, State에 담겨있는 배열값을 반복돌면서 JSX출력하는 패턴

--- src/components/sub/Gallery.jsx
+++ src/components/sub/Gallery.jsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from "react";
 import Layout from "../common/Layout";
 
 export default function Gallery() {
+	const [Flickr, setFlickr] = useState([]);
+	console.log(Flickr);
+
+	useEffect(() => {
+		// people에 flickr.people.getPhotos메서드 연결
+		const method = "flickr.people.getPhotos";
+		const flickr_api = import.meta.env.VITE_FLICKR_API;
+		const myID = "201494903@N03";
+		const num = 10;
+		const url = `https://www.flickr.com/services/rest/?api_key=${flickr_api}&method=${method}&user_id=${myID}&per_page=${num}&nojsoncallback=1&format=json`;
+		fetch(url)
+			.then(data => data.json())
+			.then(json => {
+				console.log(json.photos.photo);
+			});
+	}, []);
+
 	return (
 		<Layout title={"GALLERY"}>
-			<p>Gallery Page Contents Come Here.</p>
+			<section className="galleryList">
+				{Flickr.map((data, idx) => {
+					return (
+						<article key={idx}>
+							<h3>{data.title}</h3>
+						</article>
+					);
+				})}
+			</section>
 		</Layout>
 	);
 }
+
+/*
+useState, useEffect 훅을 활용해서 외부 서버데이터를 가져오고 컴포넌트에 렌더링하는 패턴
+1. 외부데이터를 담을 State와 State변경함수를 useState로 부터 생성
+2. 의존성 배열이 비어있는 useEffect구문 생성 (서버데이터는 컴포넌트 초기 렌더리시 한번만 가져오는 것이 일반적)
+3. useEffect구문 안쪽에서 데이터를 요청 URL을 생성하기 위한 정보값 변수에 담기
+4. useEffect구문 안쪽에서 완성된 요청 URL로 fetch함수를 통해 데이터 요청
+4-1.만약 제대로 url요청을 했음에도 불구하고 콘솔에러로 'not Valid JSON'에러 뜰시 다음의 쿼리스트링 옵션을 뒤에 추가
+4-2. nojsoncallback=1&format=json;
+5. fetch함수의 then구문 안에서 전달받은 서버데이터로부터 배열만 뽑아서 미리 준비해놓은 State변경함수로 담기
+6. return문 안쪽에서 State값을 map으로 반복돌며 원하는 형태의 JSX로 출력
+*/
