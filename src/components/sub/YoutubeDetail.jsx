f5ac97aa06dd140f82016ac426b6b14c9e4e9eb7
universe119 <ruddh119@gmail.com>
2024-10-07 09:31:16 +0900

    동적 라우터 주석 설명 추가

--- src/components/sub/YoutubeDetail.jsx
+++ src/components/sub/YoutubeDetail.jsx
@@ -9,12 +9,14 @@ export default function YoutubeDetail() {
 
 	// useParams로 url을 통해 전달되는 파라미터값을 반환
 	// 주소/youtube/abc (abc라는 값을 params 객체로 전달 받음)
+	console.log('detail');
 	const { id } = useParams();
 	const [YoutubeVid, setYoutubeVid] = useState(null);
 	const combineText = useCombineText();
 	console.log(YoutubeVid);
 
 	useEffect(() => {
+		console.log('detail func');
 		// 이전 목록화면에서 제목 클릭시 전달되는 id값을 param로 받아서
 		//새로운 요청 url을 만들고 useEffect로 컴포넌트 마운트시 한번만 서버쪽에 데이터 요청 후 배열값 전달 받음
 		const api_key = import.meta.env.VITE_YOUTUBE_API;
