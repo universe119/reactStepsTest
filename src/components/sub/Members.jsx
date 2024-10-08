9363e6fde5faec2e36d99ff9810278c2816fb5db
universe119 <ruddh119@gmail.com>
2024-10-08 10:16:29 +0900

    리액트에서 직접적으로 리얼돔(html Dom)을 제어하면 안되는 이유

--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -3,6 +3,16 @@ import memberData from "../../data/memberData";
 import Pic from "../common/Pic";
 
 export default function Members() {
+	// h2가상돔 요소 클릭
+	const changeColor = () => {
+		// changeColor 함수가 호출되는 순간 가상돔 요소를 찾는 것이 아닌
+		// 이전 렌더링 사이클 때 변환된 리얼돔을 직접 가져와서 스타일 변경
+		// 이처럼 가상돔이 아닌 이전 렌더링 타임에 생성된 리얼돔을 직접 제어하면 안되는 이유
+		// 문제점1: 리액트에서 중요한 정보로 취급하는 state와 연관이 없는 일반 html DOM요소를 제어하기 때문에 추후 데이터 추적 불가능
+		// 문제점2 : 현재 렌더링 사이클에서 다루고 있는 최신 요소가 아닌 이전 렌더링때 생성된 요소를 다루기 때문에 잘못된 예전 데이터를 다루게 됨
+		const pEl = document.querySelector(".titBox p");
+		pEl.style.color = "red";
+	};
 	return (
 		<Layout title={"MEMBERS"}>
 			{/* 첫번째 데이터만 뽑아서 출력 */}
@@ -16,7 +26,7 @@ export default function Members() {
 
 			<article className="memberListBox">
 				<div className="titBox">
-					<h2>Our Team Members</h2>
+					<h2 onClick={changeColor}>Our Team Members</h2>
 					<p>
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora possimus non ipsa cum. Veritatis, dolore
 						aliquam? Consectetur assumenda dolor labore.
