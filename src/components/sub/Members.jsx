be104faa95a972089ed8fa43a54e8230205dce6f
universe119 <ruddh119@gmail.com>
2024-10-08 10:28:11 +0900

    useRef를 통한 참조객체로 가상돔 요소 제어

--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -1,18 +1,33 @@
 import Layout from "../common/Layout";
 import memberData from "../../data/memberData";
 import Pic from "../common/Pic";
+import { useRef } from "react";
 
+/*
+	참조객체에 가상돔을 담아 활용하는 패턴
+	1. useRef로 빈 참조객체 개성
+	2. 원하는 가상돔요소(JSX El) ref속성으로 참조객체 연결
+	3. 참조객체명.current 로 해당요소를 가져와서 제어
+*/
 export default function Members() {
 	// h2가상돔 요소 클릭
+	const pEl = useRef(0);
+	console.log(pEl);
+
 	const changeColor = () => {
+		console.log(pEl);
+
 		// changeColor 함수가 호출되는 순간 가상돔 요소를 찾는 것이 아닌
 		// 이전 렌더링 사이클 때 변환된 리얼돔을 직접 가져와서 스타일 변경
 		// 이처럼 가상돔이 아닌 이전 렌더링 타임에 생성된 리얼돔을 직접 제어하면 안되는 이유
 		// 문제점1: 리액트에서 중요한 정보로 취급하는 state와 연관이 없는 일반 html DOM요소를 제어하기 때문에 추후 데이터 추적 불가능
 		// 문제점2 : 현재 렌더링 사이클에서 다루고 있는 최신 요소가 아닌 이전 렌더링때 생성된 요소를 다루기 때문에 잘못된 예전 데이터를 다루게 됨
-		const pEl = document.querySelector(".titBox p");
-		pEl.style.color = "red";
+		// 브라우저 상에서 끌어옴
+		// const pEl = document.querySelector(".titBox p");
+		// 참조객체의 가상돔을 제어하면 현재 렌더링 사이클의 초신 가상돔 정보를 제어가능(관련 내용은 렉시컬 스코프랑 고차함수 카페에서 보자.)
+		pEl.current.style.color = "red";
 	};
+
 	return (
 		<Layout title={"MEMBERS"}>
 			{/* 첫번째 데이터만 뽑아서 출력 */}
@@ -27,7 +42,8 @@ export default function Members() {
 			<article className="memberListBox">
 				<div className="titBox">
 					<h2 onClick={changeColor}>Our Team Members</h2>
-					<p>
+					{/* 미리 생성한 빈 참조객체에 담고 싶은 가상돔요소에 ref속성으로 연결 */}
+					<p ref={pEl}>
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora possimus non ipsa cum. Veritatis, dolore
 						aliquam? Consectetur assumenda dolor labore.
 					</p>
