a1bfabe3eaf60103b748fda091a68ad249579a74
universe119 <ruddh119@gmail.com>
2024-10-08 14:13:53 +0900

    함수의 복잡한 구조의 인수 전달방법

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -12,7 +12,7 @@ export default function Layout({ title, children }) {
 
 	useEffect(() => {
 		// 전달한 인수가 3개 이상일때는 객체형식으로 전달
-		splitText(ref_title, 0.1, 0.1);
+		splitText(ref_title, { interval: 0.1, delay: 0.1 });
 	}, []);
 
 	return (
--- src/hooks/useSplitText.js
+++ src/hooks/useSplitText.js
@@ -8,11 +8,15 @@
 // 렉시컬 스코프 환경 : 안쪽의 지역변수가 계속 값이 유지되는 현상
 
 export default function useSplitText() {
-	//내부적으로 current값을 직접 추출하기때문에 호출시 참조객체명만 전달
-	return (ref, interval = 0, delay = 0) => {
-		//인수로 전달받은 참조객체안의 요소의 텍스트만 가져옴
+	return (ref, option) => {
+		if (!ref) return console.error("첫번째 인수로는 참조객체값이 와야 됩니다.");
+		const default_option = { interval: 0, delay: 0 };
+
+		//불변성을 유지하면서 2개의 객체를 합쳐서 새로운 객체 반환하는 패턴
+		//{...객체1, ...객체2}
+		const result_option = { ...default_option, ...option };
 		let text = ref.current.innerText;
-		console.log(ref);
+		// console.log(ref);
 
 		//동적으로 생성될 태그문자열이 담길 빈 변수 생성
 		let tags = "";
@@ -20,7 +24,7 @@ export default function useSplitText() {
 		//문자열을 반복돌면서 동적으로 <span>으로 감싸면서 문자열 쌓아나감
 		for (let letter of text) {
 			tags += `<span style='display:inline-block; transition-duration:0.5s; transition-delay:${
-				count * interval + delay //반복순번 * 모션시간 간격, 초기 지연시간
+				count * result_option.interval + result_option.delay //반복순번 * 모션시간 간격, 초기 지연시간
 			}s'>${letter}</span>`;
 			count++;
 		}
