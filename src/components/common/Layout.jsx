1d86937035cf93d96f3b18722e7a56adcb96bdbb
universe119 <ruddh119@gmail.com>
2024-10-10 13:32:36 +0900

    텍스트 분리 문자 모션 컴포넌트 생성(사용성, 성능 개선)

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,35 +1,22 @@
 import { useLocation } from "react-router-dom";
-import useSplitText from "../../hooks/useSplitText";
-import { useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import MaskText from "./MaskText";
 import Mask from "./Mask";
+import SplitText from "./SplitText";
 
 export default function Layout({ title, children }) {
-	//커스텀훅으로 핸들러함수 안쪽에서 호출할 수 있는 실제사용가능한 함수 반환 받음
-	const ref_title = useRef(null);
-
-	const splitText = useSplitText();
 	const { pathname } = useLocation();
 	const isDetail = pathname.includes("/youtube/");
-
-	useEffect(() => {
-		// 전달한 인수가 3개 이상일때는 객체형식으로 전달
-		// 마스크 순번2 - 마스크 모션이 끝날때 바로 제목 타이핑 모션
-		splitText(ref_title, { interval: 0.1, delay: 0.3 });
-		// useEffect에 의존성 배열에 특정 값을 등록하라고 뜨는 경우
-		// 해당 컴포넌트자제척으로 제어되지 않은 요소가 useEffect안쪽에서 활용되고 있을 때 등록하라는 권공 사항 출력
-		// 해결방법 : 등록 처리(잘못등록하면 재귀적호출 되면서 무한 호출문제)
-		// 무한호출시 해결방법:useMemo, useCallback등의 메모리제이션 훅을 이용해서 강제로 메모리에 등록 후 사용
-	}, []);
-
 	return (
 		<>
 			<main className={isDetail ? "detail" : title.toLowerCase()}>
 				{/* <motion.h1 ref={ref_title} exit={{ opacity: 0, transition: { delay: 0 } }}>
 					{title}
-				</motion.h1> */}
-				<h1 ref={ref_title}>{title}</h1>
+				</motion.h1> 내가 잠깐 만들어봤지만 성능면에서 별로였던 코드*/}
+				{/* <h1 ref={ref_title}>{title}</h1>
+				 */}
+				<SplitText delay={0.5}>{title}</SplitText>
+				<br />
 
 				{/* 마스크 순번3 - 텍스트 타이핑 모션 끝날 시점에 첫줄 텍스트 마스크 모션 시작 */}
 				<MaskText delay={1} color={"#444"} style={{ fontSize: 20, fontFamily: "arial" }}>
---
+++ src/components/common/SplitText.jsx
@@ -0,0 +1,46 @@
+import { motion } from "framer-motion";
+
+export default function SplitText({ children, style, interval = 0.1, delay = 0, duration = 0.3 }) {
+	const textArr = [];
+	for (const letter of children) textArr.push(letter);
+
+	const titStyle = {
+		display: "inline-block",
+		marginBottom: 50,
+		fontWeight: 100,
+		fontSize: "6vmax",
+		fontFamily: "raleway",
+		lineHeight: 1,
+		color: "#333",
+		...style
+	};
+	const { init, active } = {
+		init: { scale: 2, opacity: 0 },
+		active: { scale: 1, opacity: 1 }
+	};
+
+	return (
+		<h2 style={titStyle}>
+			{textArr.map((el, idx) => (
+				<motion.span
+					style={{ display: "inline-block" }}
+					key={idx}
+					initial={init}
+					animate={active}
+					transition={{ duration: duration, delay: interval * idx + delay }}>
+					{el}
+				</motion.span>
+			))}
+		</h2>
+	);
+}
+
+/*
+	미션 (1시 30분까지 제작)
+	- 기존 useSplitText라는 커스텀 훅으로 문자열 반복생성하는 문제점 파악 (리얼돔 제어, useEffect를 통해 리얼돔에 다시 클래스를 제어하는 모션 형태(비권장 방식)  )
+	- 아예 span요소르 분리된 새로운 가상돔을 반환하는 형태로 컴포넌트 함수 제작 (리액트 개발 의도에 부합됨)
+	- 순서1 : children으로 전달받은 문자값을 컴포넌트 내부에서 배열로 변환
+	- 순서2 : 변환된 문자열 배열을 반복돌며 span가상돔 요소로 반복 생성
+	- 순서3 : 반복생성될때 scss파일이 아닌 스타일 객체를 직접 연결해 재사용성 올림
+	- 순서4 : props를 전달하여 지연시간, 스타일 정보등을 호출시에 정할수 있도록 개발 편의성 고도화
+*/
